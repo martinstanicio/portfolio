09ffc1f31c033ad722b25245c9bd30c7006907ef
Martín Stanicio <martuytimo@gmail.com>
2023-05-11 22:49:26 -0300

    add social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,17 @@
 export default function Footer() {
   return (
     <footer>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/martinstanicio/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/martinstanicio">Instagram</a>
+        </li>
+        <li>
+          <a href="https://www.github.com/martinstanicio">GitHub</a>
+        </li>
+      </ul>
       <p>Martín Stanicio © {new Date().getFullYear()}</p>
     </footer>
   );

845210300d08915d16f3eb70066ed9e858535ce4
Martín Stanicio <martuytimo@gmail.com>
2023-11-04 00:35:10 -0300

    install contentlayer

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function removeExtension(filename: string) {
+  return filename.substring(0, filename.lastIndexOf(".")) || filename;
+}

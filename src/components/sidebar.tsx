7c0abd37ea9b687cd10459f2f6d80584ebcadbff
Martín Stanicio <martuytimo@gmail.com>
2023-11-11 17:19:08 -0300

    adjust sidebar content vertical alignment

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -17,7 +17,7 @@ export default function Sidebar({ className, ...props }: Props) {
   return (
     <section
       className={cn(
-        "sticky top-0 flex h-screen w-full max-w-sm flex-col justify-between gap-8 overflow-y-auto bg-secondary p-12 text-secondary-foreground",
+        "sticky top-0 h-screen w-full max-w-sm space-y-8 overflow-y-auto bg-secondary p-12 text-secondary-foreground",
         className,
       )}
       {...props}

594f97e32d8a11256b670f165897d948b31367d9
Martín Stanicio <martuytimo@gmail.com>
2023-11-11 19:13:25 -0300

    disable contentlayer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const { withContentlayer } = require("next-contentlayer");
+// const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withContentlayer(nextConfig);
+// module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
